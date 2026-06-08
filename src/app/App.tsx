6d3674acbcdd9c0f82264dc5ff0f22da2dccbb7e
Jan Carlo <jcmcardama@gmail.com>
2026-06-08 23:49:15 +0800

    add scroll top comp

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppStyling } from './AppStyling';
 import AppRoutes from './routes/AppRoutes';
+import ScrollToTop from './components/ScrollToTop';
 
 export default function App() {
   return (
     <AppStyling>
       <BrowserRouter>
+        <ScrollToTop />
         <AppRoutes />
       </BrowserRouter>
     </AppStyling>
