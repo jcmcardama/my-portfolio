e0cecd52f68aaf480983315330a8e4d35a28892f
Jan Carlo <jcmcardama@gmail.com>
2026-06-16 04:06:14 +0800

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
 import App from './app/App';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
